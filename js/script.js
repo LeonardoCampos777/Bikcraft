45f127b51f1a57a4606b8fbb6de218e78a39a5f0
Leonardo Campos <leonardo.kiss66@live.com>
2023-08-23 20:06:22 -0300

    update

--- js/script.js
+++ js/script.js
@@ -18,13 +18,14 @@ links.forEach(ativarLink);
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro){
-  const elemento = document.getElementById(parametro);
+  elemento = document.getElementById(parametro);
 
   if(elemento){
     elemento.checked = true;
   }
- 
-  console.log(elemento);
+
 }
 
 parametros.forEach(ativarProduto);
+
+
