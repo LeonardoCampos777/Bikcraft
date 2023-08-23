4735c39afbc07d1ac1d78be2c05e7425587ce4e9
Leonardo Campos <leonardo.kiss66@live.com>
2023-08-23 12:52:56 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
+// ATIVAR LINKS DO MENU
 const links = document.querySelectorAll('.header-menu a');
-
 
 function ativarLink(link){
   const url = location.href;
   const href = link.href;
 
-
+  
   if (url.includes(href)){
     link.classList.add('ativo');
   }
 }
 
 links.forEach(ativarLink);
+
+// ATIVAR INTENS DO ORÇAMENTO
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro){
+  const elemento = document.getElementById(parametro);
+
+  if(elemento){
+    elemento.checked = true;
+  }
+ 
+  console.log(elemento);
+}
+
+parametros.forEach(ativarProduto);
