84b2d12ede4a407afbdb912efba6d2c970f20f29
Leonardo Campos <leonardo.kiss66@live.com>
2023-08-31 19:17:44 -0300

    update

--- js/script.js
+++ js/script.js
@@ -54,11 +54,12 @@ const galeriaContainer = document.querySelector('.bicicleta-imagens');
 
 function trocarImagem(event){
   const img = event.currentTarget;
-  const media = matchMedia('(min-width: 1000px)').matches;
+  const media = matchMedia('(min-width:1000px)').matches;
 
-  if (media){
+  if(media){
     galeriaContainer.prepend(img);
   }
+
 }
 
 function eventosGaleria(img){
@@ -66,3 +67,9 @@ function eventosGaleria(img){
 }
 
 galeria.forEach(eventosGaleria);
+
+
+// ANIMAÇÃO
+if (window.SimpleAnime){
+  new SimpleAnime(); 
+}
